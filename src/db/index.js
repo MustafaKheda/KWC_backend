4b110a3d9d0d28820caaa275225c262b7d6e72e7
Mustafa Kheda <mustafa.kheda@kadellabs.com>
2024-08-08 12:10:45 +0530

    improvement

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      process.env.MONGO_URL
+      `${process.env.MONGO_URL}/beautifyBackend`
     );
     console.log("\n MongoDB Connected: ", connectionInstance.connection.host);
   } catch (err) {
